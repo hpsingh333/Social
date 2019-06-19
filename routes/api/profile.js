4d4f1ac7daccbeb90c03e3dd9b46799cba4dd967
Harman Preet Singh <92.harmanpreet@gmail.com>
2019-06-19 23:39:10 +0530

    Delete user by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -149,4 +149,19 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+//@route    DELETE api/profile
+//@desc     Delete profile, users and posts
+//@access   Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User Deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
